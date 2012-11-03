04fe39d393b716248424c0427af673e9b65f4c85
mwkuster <marc@budabe.eu>
2012-11-03 11:03:27 +0100

    ATMOS signature implementation added

--- src/s6/atmos.clj
+++ src/s6/atmos.clj
@@ -5,8 +5,11 @@
            (javax.crypto Mac)
            (javax.crypto.spec SecretKeySpec))
   (:require [clojure.string :as s]
-            [clojure.data.codec.base64 :as base64]))
-;            [net.djpowell.b64.core]))
+            [clojure.data.codec.base64 :as base64]
+            [clj-http.client :as client]
+            [clj-time.core :as time]
+            [clj-time.local :as local]
+            [clj-time.format :as format]))
 
 (defn canonicalize-emc-headers [headers]
   "Bring the x-emc specific extension headers into a canonical form"
@@ -45,3 +48,83 @@ return hashout"
      (Base64/encodeBase64String 
       (.doFinal mac 
                 (.getBytes hashstring))))))
+
+(defn get-date-time []
+  "Get the current datetime in RFC822 format (the one required by http)"
+  ;:rfc822
+  (local/format-local-time (time/now) :rfc822))
+
+(defn create-object [uuid secret-key user-id content listable-metadata]
+  "Create an object in EMC2"
+  (let
+      [headers {
+                "content-type"  "application/octet-stream",
+                "x-emc-uid"  uuid,
+                "x-emc-listable-meta" listable-metadata,
+                "x-emc-date"  (get-date-time),
+                "x-emc-useracl" user-id,
+                }
+       signature (sign secret-key "POST" "/rest/objects" headers)
+       headers-with-signature (assoc headers "x-emc-signature" signature)]
+    (println headers-with-signature)
+    (client/post "https://api.atmosonline.com/rest/objects"
+                 {:body content 
+                  :accept :json
+                  :headers headers-with-signature}))) 
+
+(defn create-object-in-namespace [uuid secret-key user-id dir content listable-metadata]
+  "Create an object in EMC2 in a logical namespace"
+  (let
+      [headers {
+                "content-type"  "application/octet-stream",
+                "x-emc-uid"  uuid,
+                "x-emc-listable-meta" listable-metadata,
+                "x-emc-date"  (get-date-time),
+                "x-emc-useracl" user-id,
+                }
+       path (str "/rest/namespace" dir)
+       signature (sign secret-key "POST" path  headers)
+       headers-with-signature (assoc headers "x-emc-signature" signature)]
+    (println headers-with-signature)
+    (client/post (str "https://api.atmosonline.com/" path)
+                 {:body content 
+                  :accept :json
+                  :headers headers-with-signature}))) 
+
+(defn get-content [uuid secret-key object-id]
+  "Get back the content for an object"
+  (let
+      [headers {
+                "content-type"  "application/octet-stream",
+                "x-emc-uid"  uuid,
+                "x-emc-date"  (get-date-time),
+                }]
+    (client/get (str "https://api.atmosonline.com/rest/objects/" object-id) 
+                {:headers (assoc headers "x-emc-signature"
+                                 (sign secret-key "GET" (str "/rest/objects/" object-id) headers))})))
+
+(defn get-system-metadata [uuid secret-key object-id]
+  "Get system-level metadata for an object"
+  (let
+      [headers {
+                "content-type"  "application/octet-stream",
+                "x-emc-uid"  uuid,
+                "x-emc-date"  (get-date-time),
+                }
+       path (str "/rest/objects/" object-id "?metadata/system")]
+    (client/get (str "https://api.atmosonline.com" path)
+                {:headers (assoc headers "x-emc-signature"
+                                 (sign secret-key "GET" path headers))})))
+
+(defn get-user-metadata [uuid secret-key object-id]
+  "Get system-level metadata for an object"
+  (let
+      [headers {
+                "content-type"  "application/octet-stream",
+                "x-emc-uid"  uuid,
+                "x-emc-date"  (get-date-time),
+                }
+       path (str "/rest/objects/" object-id "?metadata/user")]
+    (client/get (str "https://api.atmosonline.com" path)
+                {:headers (assoc headers "x-emc-signature"
+                                 (sign secret-key "GET" path headers))})))
