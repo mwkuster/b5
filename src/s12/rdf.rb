ef2419f78d108a3d12d08762dc854eb2a9e6b4b2
Marc Wilhelm Küster <kuester@client-181-159.eduroam.fh-worms.de>
2014-01-03 10:01:01 +0100

    Lecture on semantic web services

---
+++ src/s12/rdf.rb
@@ -0,0 +1,58 @@
+require 'rdf'
+require 'sparql'
+require 'rdf/turtle'
+require 'rdf/rdfxml'
+
+#Named graph
+repo = RDF::Repository.new
+repo.load("62007CJ0535.rdf", :format => :rdfxml)
+repo.load("61960CC0002.ttl", :format => :turtle)
+
+#Just for debugging: show the contents of the repository
+repo.each_statement { |statement| puts statement }
+
+
+def output_all_triples (repository)
+   query = <<-sparql
+PREFIX cdm: <http://publications.europa.eu/ontology/cdm#> 
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+SELECT ?s ?p ?o
+WHERE {
+ ?s ?p ?o
+}
+sparql
+  puts query
+              
+  solutions = SPARQL.execute(query, repository)
+  puts "All triples:"
+  solutions.each { |sol| puts sol.to_hash }
+  nil
+end
+
+def titles_by_celex (repo, celex)
+  "Returns all the titles and their languages for a given celex"
+   query = <<-sparql
+PREFIX cdm: <http://publications.europa.eu/ontology/cdm#> 
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+SELECT ?expr ?title ?lang
+WHERE
+{
+ ?work cdm:resource_legal_id_celex  "#{celex}"^^xsd:string .
+ ?expr cdm:expression_belongs_to_work ?work .
+ ?expr cdm:expression_title ?title .
+ ?expr cdm:expression_uses_language ?lang .
+}
+sparql
+  puts query
+              
+  solutions = SPARQL.execute(query, repo)
+  puts "Titles:"
+  puts solutions.each { |sol| puts sol.to_hash}
+
+  solutions.collect { |sol| sol[:title].to_s}
+end
+
+output_all_triples(repo)
+
+titles = titles_by_celex(repo, "62007CJ0535")
+puts titles
